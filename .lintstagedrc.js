b50c8fc0c28e43b4b6ca9ab2497c9bc281965ee7
AVex3nder <alex.vila.dev@gmail.com>
2022-01-29 15:42:53 +0100

    build: init

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const buildEslintCommand = filenames =>
+  `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
+module.exports = {
+  // Run ESLint on changes to JavaScript/TypeScript files
+  'src/**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  // Run type-check on changes to TypeScript files
+  'src/**/*.ts?(x)': filenames => `tsc-files --pretty --noEmit ${filenames.join(' ')}`,
+  // Prettify on changes to JavaScript/TypeScript files
+  'src/**/*.(ts|js)?(x)': filenames => `yarn prettier --write . ${filenames.join(' ')}`,
+};
