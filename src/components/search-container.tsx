38ffe99e45565e5295de7e343a756d2706135091
LeoEmVee <santito79@hotmail.com>
2022-02-02 19:41:06 +0100

    Add toggle functionality to shopping/eating buttons (to be debugged yet)

--- src/components/search-container.tsx
+++ src/components/search-container.tsx
@@ -1,19 +1,38 @@
 import Link from 'next/link';
 import React from 'react';
 import SearchBar from './search-bar';
-
-// interface IFSearchContainer {
-//   prop: any;
-// }
+import { useAppSelector, useAppDispatch } from '../redux/store';
+import {
+  onClickEating,
+  onClickShopping,
+} from '../redux/actions/homePageSearchActions';
 
 function SearchContainer() {
+  const { eating, shopping } = useAppSelector(state => state.homePageSearch);
+
+  const dispatch: Function = useAppDispatch();
+
+  const toggleEating = () => {
+    dispatch(onClickEating());
+    console.log(eating);
+  };
+
+  const toggleShopping = () => {
+    dispatch(onClickShopping());
+    console.log(shopping);
+  };
+
   return (
     <div className="search-container">
       <div>- Search Container</div>
       <SearchBar />
       <span>* -- </span>
-      <button type="button">Shopping (filter)</button>
-      <button type="button">Eating (filter)</button>
+      <button type="button" onClick={toggleShopping}>
+        Shopping (filter)
+      </button>
+      <button type="button" onClick={toggleEating}>
+        Eating (filter)
+      </button>
       <Link href="/add-content" passHref>
         <button type="button">Add Item (link)</button>
       </Link>
