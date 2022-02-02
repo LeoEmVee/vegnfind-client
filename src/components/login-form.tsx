4a2507b1011ca58e79c6d4ff74ce5e0357b8c6a2
AVex3nder <alex.vila.dev@gmail.com>
2022-02-02 11:05:29 +0100

    working on loading modal and routing on submit login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { submitLoginForm } from '../services/axios.service';
-
-// interface IFFormComponent {
-//   prop: any;
-// }
+import LoadingModal from './loading-modal';
 
 function LoginForm() {
-  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const validation = Yup.object({
     username: Yup.string().min(8).required('username is required'),
@@ -18,64 +16,70 @@ function LoginForm() {
   });
   validation.validate;
 
-  async function submitLogin(data: any) {
-    console.log(data);
-    const token = await submitLoginForm(data);
-    if (token) {
+  async function submitLogin(formData: any) {
+    setIsLoading(true);
+    console.log('loading?', isLoading);
+    const { access_token } = (await submitLoginForm(formData)).data;
+    if (access_token) {
       setIsAuthorized(true);
-      console.log('authorized', isAuthorized, token);
+      window.localStorage.access_token = access_token;
     } else {
-      console.log('Unauthorized');
+      setIsAuthorized(false);
     }
+    setIsLoading(false);
+    console.log('loading?', isLoading);
   }
 
   return (
-    <Formik
-      initialValues={{
-        username: '',
-        password: '',
-      }}
-      validationSchema={validation}
-      onSubmit={(values, { resetForm }) => {
-        submitLogin(JSON.stringify(values, null, 2));
-        // console.log(JSON.stringify(values, null, 2));
-        resetForm();
-      }}>
-      {formik => (
-        <form className="login-form" onSubmit={formik.handleSubmit}>
-          <label htmlFor="username">User name</label>
-          <div>
-            <input
-              id="username"
-              name="username"
-              type="username"
-              placeholder="What is your username?"
-              onChange={formik.handleChange}
-              value={formik.values.username}
-            />
-            {formik.touched.username && formik.errors.username ? (
-              <div>{formik.errors.username}</div>
-            ) : null}
-          </div>
+    <>
+      {isLoading && <LoadingModal />}
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+        }}
+        validationSchema={validation}
+        onSubmit={(values, { resetForm }) => {
+          submitLogin(JSON.stringify(values, null, 2));
+          // console.log(JSON.stringify(values, null, 2));
+          resetForm();
+        }}>
+        {formik => (
+          <form className="login-form" onSubmit={formik.handleSubmit}>
+            <label htmlFor="username">User name</label>
+            <div>
+              <input
+                id="username"
+                name="username"
+                type="username"
+                placeholder="What is your username?"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+              />
+              {formik.touched.username && formik.errors.username ? (
+                <div>{formik.errors.username}</div>
+              ) : null}
+            </div>
 
-          <label htmlFor="password">Password</label>
-          <div>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-            />
-            {formik.touched.password && formik.errors.password ? (
-              <div>{formik.errors.password}</div>
-            ) : null}
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      )}
-    </Formik>
+            <label htmlFor="password">Password</label>
+            <div>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+              ) : null}
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
+    </>
   );
 }
 
