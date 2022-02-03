3bb7672333e701401326928391bba1886d628063
Oriol Cervantes Sánchez <oriolcesa@hotmail.com>
2022-02-03 16:18:09 +0100

    Ket on working on styles

--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -2,10 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { useAppSelector } from '../redux/store';
+import styles from './user-card.module.css'
 
-// interface IFUserCard {
-//   name: any;
-// }
 
 function UserCard() {
 
@@ -13,18 +11,21 @@ function UserCard() {
 
 
   return (
-    <div className="user-card">
-      {/* <Image src={profilePic} width="200" height="200" alt="Your profile pic"></Image>
+    <div className={styles.usercardwrap}>
+      <div className={styles.profilepicwrap}>
+        {/* <Image src={profilePic} width="200" height="200" alt="Your profile pic"></Image>
       <h1>Hi {firstName}</h1> */}
-      <p>57 reviews</p>
-      <p>99 years</p>
-      <p>he/him</p>
-      <p>Barcelona, Spain</p>
-      <p>Member since Oct 2021</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae impedit autem rerum at quisquam perferendis cum officiis tempore ea, sit quae! Illo, molestias? Odio tenetur quod, enim doloremque eligendi consectetur.</p>
-
+      </div>
+      <div className={styles.detailswrap}>
+        <p>57 reviews</p>
+        <p>99 years</p>
+        <p>he/him</p>
+        <p>Barcelona, Spain</p>
+        <p>Member since Oct 2021</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae impedit autem rerum at quisquam perferendis cum officiis tempore ea, sit quae! Illo, molestias? Odio tenetur quod, enim doloremque eligendi consectetur.</p>
+      </div>
       <Link href="/add-content" passHref>
-        <a>Add a new product, shop or restaurant</a>
+        <button>Add a new product, shop or restaurant</button>
       </Link>
     </div>
   );
