5289ae652391f74a6a808113654de00020edf31d
LeoEmVee <santito79@hotmail.com>
2022-02-04 17:08:15 +0100

    Add global results rendering to results-list/item

--- src/components/results/result-item.tsx
+++ src/components/results/result-item.tsx
@@ -6,7 +6,7 @@ import FullVgnBigFlag from '../../assets/flags/flag-full-vegan-big.svg';
 import RatingFull from '../../assets/icons/icon-ratingpoint-full.svg';
 import StarEmpty from '../../assets/icons/icon-star-empty.svg';
 
-function ResultItem() {
+function ResultItem({ itemDetails }) {
   return (
     <Link href="/item-detail" passHref>
       <div className={styles.resultitemwrap}>
@@ -14,9 +14,9 @@ function ResultItem() {
         <div className={styles.itemdetailswrap}>
           <div className={styles.itempiccontainer}></div>
           <div className={styles.itemdescriptionwrap}>
-            <h3>Not Only Salads</h3>
-            <h4>Eating, Mediterranean</h4>
-            <p>Barcelona, Catalunya, Spain</p>
+            <h3>{itemDetails.name}</h3>
+            <h4>{itemDetails.description}</h4>
+            <p>Barcelona, Catalunya (STILL MOCK)</p>
           </div>
           <div className={styles.ratingcontainerwrap}>
             <RatingFull className={styles.ratingitem} />
@@ -25,7 +25,7 @@ function ResultItem() {
             <RatingFull className={styles.ratingitem} />
             <RatingFull className={styles.ratingitem} />
           </div>
-          <p className={styles.reviewcounter}>20 reviews</p>
+          <p className={styles.reviewcounter}>20 reviews (STILL MOCK)</p>
           <button className={styles.favouritebutton} type="button">
             <StarEmpty />
           </button>
--- src/components/results/results-list.tsx
+++ src/components/results/results-list.tsx
@@ -3,16 +3,17 @@ import Link from 'next/link';
 import Pagination from '../pagination';
 import ResultItem from './result-item';
 import styles from '../results/results-list.module.css';
+import { useAppSelector } from '../../redux/store';
 
 function ResultsList() {
+  const { searchResults } = useAppSelector(state => state.searchReducer);
   return (
     <div className={styles.resultslistwrap}>
       <Pagination />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
-      <ResultItem />
+      {searchResults.length &&
+        searchResults.map(result => {
+          return <ResultItem key={result.id} itemDetails={result} />;
+        })}
       <Link href="#top" passHref>
         <button className={styles.backtotopbutton}>Top</button>
       </Link>
