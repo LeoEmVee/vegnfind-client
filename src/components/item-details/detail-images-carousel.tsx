d83483c22045735be50c63fce11a622010734be4
Oriol Cervantes Sánchez <oriolcesa@hotmail.com>
2022-02-05 17:12:14 +0100

    Styled item page

--- src/components/item-details/detail-images-carousel.tsx
+++ src/components/item-details/detail-images-carousel.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-
-// interface IFDetailImagesCarousel {
-//   prop: any;
-// }
+import styles from './detail-images-carousel.module.css'
 
 function DetailImagesCarousel() {
   return (
-    <div className="detail-images-carousel">
-      <div>- Images Carousel</div>
+    <div className={styles.carouselwrap}>
+      <img src="https://res.cloudinary.com/daq7z9fqb/image/upload/v1643880889/w2zqalie6yint1jl9z7m.jpg" alt="bla" />
+      <img src="https://res.cloudinary.com/daq7z9fqb/image/upload/v1643883841/xlrkcu5vzxwemio6m5j3.jpg" alt="" />
+      <img src="https://res.cloudinary.com/daq7z9fqb/image/upload/v1643832630/uggfttlcsxnouitrqfyr.jpg" alt="" />
+      <img src="https://res.cloudinary.com/daq7z9fqb/image/upload/v1643913893/zfexot2scs3kgmddybrr.jpg" alt="" />
+      <img src="https://res.cloudinary.com/daq7z9fqb/image/upload/v1643830192/dfqy4i9t39nccmghh7d6.jpg" alt="" />
     </div>
   );
 }
