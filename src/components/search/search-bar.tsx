5ffff67c958204fbe825ec61eb2868157f033030
Oriol Cervantes Sánchez <oriolcesa@hotmail.com>
2022-02-05 18:58:12 +0100

    Kept on fixing random css details that no body would care about but I do bc I'm sick

--- src/components/search/search-bar.tsx
+++ src/components/search/search-bar.tsx
@@ -51,8 +51,8 @@ function SearchBar({ smallBar }: IProps) {
               smallBar
                 ? styles.smallsearchbar
                 : eating || shopping
-                ? styles.shortsearchbar
-                : styles.searchbar
+                  ? styles.shortsearchbar
+                  : styles.searchbar
             }
             placeholder={
               logUser
