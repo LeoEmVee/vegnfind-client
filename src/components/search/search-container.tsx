84ccba5ae5ac0b6c66fb344e4b19ee20c65425ff
AVex3nder <alex.vila.dev@gmail.com>
2022-02-06 21:18:51 +0100

    new selectSearch component styled and implemented, working on search behaviour details

--- src/components/search/search-container.tsx
+++ src/components/search/search-container.tsx
@@ -45,7 +45,7 @@ function SearchContainer() {
         </button>
       </Link>
       <SelectSearch />
-      <SearchBar />
+      {/* <SearchBar /> */}
     </div>
   );
 }
--- src/components/search/selectSearch.tsx
+++ src/components/search/selectSearch.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import styles from './search-bar.module.css';
 import AsyncSelect from 'react-select/async';
+import styles from './search-bar.module.css';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import {
   setSearchItem,
@@ -8,6 +8,8 @@ import {
 } from '../../redux/actions/searchActions';
 import { sendSearchQuery } from '../../services/axios.service';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import SearchIcon from '../../assets/icons/icon-search.svg';
 
 interface IProps {
   smallBar?: boolean;
@@ -15,6 +17,7 @@ interface IProps {
 
 function SelectSearch({ smallBar }: IProps) {
   const [goToDetails, setGoToDetails] = useState(false);
+  const [searchCount, setSearchCount] = useState(0);
   const dispatch: Function = useAppDispatch();
   const { searchResults, searchItem, shopping, eating } = useAppSelector(
     state => state.searchReducer,
@@ -22,19 +25,51 @@ function SelectSearch({ smallBar }: IProps) {
   const { logUser } = useAppSelector(state => state.loginReducer);
   const router = useRouter();
 
+  const selectStyles = {
+    // option: (provided, state) => ({
+    //   ...provided,
+    //   borderBottom: '1px dotted pink',
+    //   color: state.isSelected ? 'red' : 'blue',
+    //   padding: 20,
+    // }),
+    control: (_: any, state: any) => ({
+      display: 'flex',
+      alignItems: 'center',
+      width: '91%',
+      marginRight: '1%',
+      height: '7vh',
+      borderRadius: '20px',
+      border: state.isFocused ? 'solid 4px var(--coral)' : 'none',
+      boxShadow: state.isFocused
+        ? '0 0 5px var(--coral)'
+        : '0 0 5px rgba(0, 0, 0, 0.2)',
+      paddingLeft: '12px',
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 700,
+      fontSize: '1.1rem',
+      backgroundColor: 'white',
+      '&:hover': {
+        boxShadow: '0 0 5px var(--coral)',
+      },
+    }),
+    placeholder: (previous: any) => ({
+      ...previous,
+      fontWeight: 400,
+      fontFamily: "'Montserrat', sans-serif",
+      fontSize: '1.1rem',
+    }),
+    // singleValue: (provided, state) => {
+    //   const opacity = state.isDisabled ? 0.5 : 1;
+    //   const transition = 'opacity 300ms';
+
+    //   return { ...provided, opacity, transition };
+    // },
+  };
+
   useEffect(() => {
     dispatch(setSearchResults([]));
     dispatch(setSearchItem(null));
   }, []);
-
-  const selectStyles = {
-    control: (): any =>
-      smallBar
-        ? styles.smallsearchbar
-        : eating || shopping
-        ? styles.shortsearchbar
-        : styles.searchbar,
-  };
 
   async function handleSearch(event: any) {
     if (!event) {
@@ -46,43 +81,77 @@ function SelectSearch({ smallBar }: IProps) {
       searchResults.some((item: any) => item.name === event)
     ) {
       setGoToDetails(true);
-      console.log('details?', goToDetails);
+      console.log('details true', goToDetails);
+      return;
     }
-    const results = (await sendSearchQuery(event))
-      .map((res: any) => res.data)
-      .flat();
-    dispatch(setSearchResults(results));
-    if (results.length === 1) {
-      dispatch(setSearchItem(results[0]));
-      setGoToDetails(true);
+    if (event.length <= 1) {
+      console.log('details false', goToDetails);
+      const results = (await sendSearchQuery(event))
+        .map((res: any) => res.data)
+        .flat();
+      await dispatch(setSearchResults(results));
+      if (results.length === 1) {
+        await dispatch(setSearchItem(results[0]));
+        setGoToDetails(true);
+        console.log('details true', goToDetails);
+        console.log('searchItem', searchItem);
+      }
     }
-    const loadResults = results.map((item: any) => {
+    const loadResults = searchResults.map((item: any) => {
       return { value: item.id, label: item.name };
     });
     return loadResults;
   }
 
   return (
-    <AsyncSelect
-      placeholder={
-        logUser
-          ? `What are you looking for, ${logUser.username}?`
-          : 'What are you looking for?'
-      }
-      cacheOptions={searchResults}
-      defaultOptions={searchResults}
-      loadOptions={handleSearch}
-      styles={selectStyles}
-      onInputChange={e => handleSearch(e)}
-      onKeyDown={e => {
-        if (e.key === 'Enter' && !goToDetails) {
-          router.push('/results-page');
-        }
-        if (e.key === 'Enter' && goToDetails) {
-          router.push(`/itemdetails/${searchItem.id}`);
-        }
-      }}
-    />
+    <div className={styles.searchbarwrap}>
+      <form className={styles.searchform}>
+        <AsyncSelect
+          // style related options
+          minMenuHeight={100}
+          maxMenuHeight={400}
+          placeholder={
+            logUser
+              ? `What are you looking for, ${logUser.username}?`
+              : 'What are you looking for?'
+          }
+          styles={selectStyles}
+          // dropdown behaviour options
+          isSearchable={true}
+          closeMenuOnSelect={true}
+          closeMenuOnScroll={true}
+          hideSelectedOptions={true}
+          loadingMessage={() => 'Searching results...'}
+          noOptionsMessage={() => null}
+          components={{
+            DropdownIndicator: () => null,
+            IndicatorSeparator: () => null,
+          }}
+          // search related options
+          cacheOptions={searchResults}
+          defaultOptions={searchResults}
+          loadOptions={handleSearch}
+          onInputChange={e => handleSearch(e)}
+          onKeyDown={e => {
+            if (e.key === 'Enter' && !goToDetails) {
+              router.push('/results-page');
+            }
+            if (e.key === 'Enter' && goToDetails) {
+              router.push(`/itemdetails/${searchItem.id}`);
+            }
+          }}
+        />
+        <Link href="/results-page" passHref>
+          {smallBar ? (
+            <button className={styles.searchiconbutton}>
+              <SearchIcon />
+            </button>
+          ) : (
+            <button className={styles.searchbarbutton}>Find</button>
+          )}
+        </Link>
+      </form>
+    </div>
   );
 }
 
