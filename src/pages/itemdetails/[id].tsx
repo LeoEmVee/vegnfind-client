0a92fcef0288979e062281f09f6305dacf8b3bae
LeoEmVee <santito79@hotmail.com>
2022-02-08 11:25:19 +0100

    Update [id] page

--- src/pages/itemdetails/[id].tsx
+++ src/pages/itemdetails/[id].tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import BackToTopButton from '../../components/back-to-top-button/back-to-top-button';
 import DetailCard from '../../components/item-details/detail-card';
 import DetailImagesCarousel from '../../components/item-details/detail-images-carousel';
@@ -5,28 +6,43 @@ import DetailMap from '../../components/item-details/detail-map';
 import Navbar from '../../components/navbar/navbar';
 import ReviewsContainer from '../../components/reviews/reviews-container';
 import ThumbnailList from '../../components/thumbnail-lists/thumbnail-list';
+import {
+  getEatById,
+  getProductById,
+  getShopById,
+} from '../../services/axios.service';
 
 function Detail({ param }: any) {
-  (async function getItem() {
-    try {
-      const eat = await getEatById(param);
-      const shop = await getShopById(param);
-      const product = await getProductById(param);
-      if (shop) return shop;
-      if (eat) return eat;
-      if (product) return product;
-    } catch (error) {
-      console.log(error);
+  const [item, setItem] = useState();
+  useEffect(() => {
+    async function init() {
+      getEatById({ id: param.id })
+        .then((res: any) => res)
+        .then((res: any) => setItem(res.data))
+        .catch((err: any) => err);
+
+      getShopById({ id: param.id })
+        .then((res: any) => res)
+        .then((res: any) => setItem(res.data))
+        .catch((err: any) => err);
+
+      getProductById({ id: param.id })
+        .then((res: any) => res)
+        .then((res: any) => setItem(res.data))
+        .catch((err: any) => err);
     }
-  })();
+    init();
+  }, []);
+  console.log('Nombre: ', item);
+
   return (
     <>
       <Navbar />
       <DetailImagesCarousel />
-      <DetailCard param={param} />
+      <DetailCard param={item} />
       <DetailMap />
       <ThumbnailList listTitle={'Products in this shop'} />
-      <ReviewsContainer param={param} />
+      <ReviewsContainer param={param} reviews={item.reviews} />
       <BackToTopButton />
     </>
   );
