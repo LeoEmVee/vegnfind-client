9c137b34551df2dbd72a3e736f8fcce9eefbcc81
LeoEmVee <santito79@hotmail.com>
2022-02-09 16:30:27 +0100

    Update results-list to filter by products, eats and/or shops

--- src/components/results/results-list.tsx
+++ src/components/results/results-list.tsx
@@ -5,14 +5,64 @@ import styles from '../results/results-list.module.css';
 import { useAppSelector } from '../../redux/store';
 
 function ResultsList() {
-  const { searchResults } = useAppSelector(state => state.searchReducer);
+  const { searchResults, eating, shopping, products } = useAppSelector(
+    state => state.searchReducer,
+  );
+
+  function filterResults() {
+    if (!eating && !shopping && !products) {
+      return (
+        searchResults.length &&
+        searchResults.map((result: any) => {
+          return <ResultItem key={result.id} itemDetails={result} />;
+        })
+      );
+    }
+    if (!eating && !shopping && products) {
+      return (
+        searchResults.length &&
+        searchResults.map((result: any) => {
+          if (!result.hasOwnProperty('isVegan'))
+            return <ResultItem key={result.id} itemDetails={result} />;
+        })
+      );
+    }
+    if (!eating && shopping && !products) {
+      return (
+        searchResults.length &&
+        searchResults.map((result: any) => {
+          if (result.hasOwnProperty('products'))
+            return <ResultItem key={result.id} itemDetails={result} />;
+        })
+      );
+    }
+    if (eating && !shopping && !products) {
+      return (
+        searchResults.length &&
+        searchResults.map((result: any) => {
+          if (
+            !result.hasOwnProperty('products') &&
+            result.hasOwnProperty('isVegan')
+          )
+            return <ResultItem key={result.id} itemDetails={result} />;
+        })
+      );
+    }
+    if (eating && shopping && !products) {
+      return (
+        searchResults.length &&
+        searchResults.map((result: any) => {
+          if (result.hasOwnProperty('isVegan'))
+            return <ResultItem key={result.id} itemDetails={result} />;
+        })
+      );
+    }
+  }
+  console.log(searchResults);
   return (
     <div className={styles.resultslistwrap}>
       <Pagination />
-      {searchResults.length &&
-        searchResults.map((result: any) => {
-          return <ResultItem key={result.id} itemDetails={result} />;
-        })}
+      {filterResults()}
     </div>
   );
 }
